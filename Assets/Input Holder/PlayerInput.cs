fd98492a028b4161ebb9dab68aa3331fd3c3603f
Jason <jason974448@gmail.com>
2025-01-13 14:55:21 -0400

    added the input system

---
+++ Assets/Input Holder/PlayerInput.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static GameInput;
+
+public class PlayerInput : IGameInputsActions
+{
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+}
